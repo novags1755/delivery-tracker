34f74f43e3a2048b93554eddcd116053b0c17578
novags1755 <rawerdl@gmail.com>
2025-05-01 19:08:14 -0700

    Cleaned up unnecessary files and directories

--- Hello.py
+++
@@ -1,4 +0,0 @@
-print("Hello, Nova")
-print("2+3=", 2+3)
-# this is a comment
-print("5*4=", 5*4)
--- input_example.py
+++
@@ -1,9 +0,0 @@
-# ask name and greet them
-name = input("What's your name?")
-print("Nice to meet you,", name)
-
-# Do a simple math operation with variables
-a = 10
-b = 5
-result = a * b 
-print("The result of", a, "x",b, "is", result)
